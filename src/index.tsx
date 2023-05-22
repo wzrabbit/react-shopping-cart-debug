25d24bba31536655c426aa3f3c4defce0a8eb06a
wzrabbit <curious.wzrabbit@gmail.com>
2023-05-22 21:35:53 +0900

    📦 chore: 홈페이지 주소 변경

--- src/index.tsx
+++ src/index.tsx
@@ -4,29 +4,24 @@ import App from "./App";
 import { RecoilRoot } from "recoil";
 import GlobalStyle from "./GlobalStyle";
 import { HashRouter } from "react-router-dom";
+import { worker } from "./mocks/browser";
 
 /**
  * 본래 mocking 서버는 배포 상태에서는 실행되어서는 안 되지만,
  * 백엔드 서버가 현재 존재하지 않고, 테스트 목적이므로 mocking 서버로 동작하도록 배포합니다.
  *
- * "false" 부분의 조건문에는 실제로 아래의 조건이 들어가야 합니다:
+ * 실제로는, 아래의 조건이 들어가야 합니다.
  * process.env.NODE_ENV !== "development"
  */
 const startMSW = async () => {
-  if (false) {
+  if (window.location.pathname === "/react-shopping-cart-debug") {
+    window.location.pathname = "/react-shopping-cart-debug/";
     return;
   }
-
-  if (window.location.pathname === "/react-shopping-cart") {
-    window.location.pathname = "/react-shopping-cart/";
-    return;
-  }
-
-  const { worker } = require("./mocks/browser");
 
   await worker.start({
     serviceWorker: {
-      url: "/react-shopping-cart/mockServiceWorker.js",
+      url: "/react-shopping-cart-debug/mockServiceWorker.js",
     },
   });
 };
